08acd28923144f50cf9f9b9620737a24526e3498
Alex Clark <aclark@aclark.net>
2017-01-21 20:18:23 -0500

    Make secret key secret; turn off debug

--- dcpython/settings.py
+++ dcpython/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_5p4**ykqgj)pdg=$rh_5)uz%sda+d*spbu3as__8b8^%gp5u%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '_5p4**ykqgj)pdg=$rh_5)uz%sda+d*spbu3as__8b8^%gp5u%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dcpython.org']
 
